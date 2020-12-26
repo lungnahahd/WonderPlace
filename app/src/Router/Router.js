f0c1be6937910d33c0062347f1fdba9ae10cc9d5
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2020-12-26 18:44:04 +0900

    #Add Router

---
+++ app/src/Router/Router.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from '../component/Home.js'
+function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/"  component={Home}/>
+            </Switch>
+        </BrowserRouter>
+
+    )
+
+}
+export default Router
