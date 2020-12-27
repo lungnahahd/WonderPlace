159daf5ea92cf8f7252f84a483e48180a925ed33
lungnahahd <jyoungjk33@gmail.com>
2020-12-27 20:17:15 +0900

    Add_MySql_Django

--- Server/Scripts/WonderPlace/settings.py
+++ Server/Scripts/WonderPlace/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'WonderPlace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myserver',
+        'PASSWORD': 'bs73017762',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
