b24651db5e0392e6d8b0461e21d3abb9f43cce09
lungnahahd <jyoungjk33@gmail.com>
2020-12-30 22:21:53 +0900

    Delete_WonderPlace

--- Server/Scripts/WonderPlace/my_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myserver',
-        'PASSWORD': 'bs73017762',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
-}
--- Server/server/main/views.py
+++ Server/server/main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# viewsets : 자주 사용하는 공통적인 view 로직을 그룹화 한 것
+#from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import WonderSerializer
+from .models import Wonder
 
-# Create your views here.
+class WonderViewSet(viewsets.ModelViewSet):
+    queryset = Wonder.objects.all()
+    serializer_class = WonderSerializer
