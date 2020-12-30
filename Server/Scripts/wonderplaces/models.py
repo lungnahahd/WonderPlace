cac339bc6d106e0f1ccc6f423d0cd55d6bf98846
lungnahahd <jyoungjk33@gmail.com>
2020-12-30 10:18:31 +0900

    Add_SampleModel

--- Server/Scripts/wonderplaces/models.py
+++ Server/Scripts/wonderplaces/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Wonder(models.Model):
+    title = models.CharField(max_length=30)
+    genre = models.CharField(max_length=15)
+    year = models.CharField()
+
+    def__str__(self):
+        return self.title
