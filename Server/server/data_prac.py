31f5e1f9d356aca857ea0b91fd7fb9a0e10d0ac5
lungnahahd <jyoungjk33@gmail.com>
2021-01-06 23:46:52 +0900

    Fix_dataprac

--- Server/server/data_prac.py
+++ Server/server/data_prac.py
@@ -2,6 +2,8 @@ import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
 import requests
+import MySQLdb
+
 
 # 인증키를 입력
 key_value ='b56169eae0274d53943d1431cc14a41a'
@@ -17,6 +19,8 @@ html = req.text
 soup = BeautifulSoup(html,'html.parser')
 # Xml 중에서 원하는 정보를 선택해서 받는 부분, 이 부분을 수정해서 원하는 정보를 골라서 받을 수 있음
 store_name = soup.find_all("bizplc_nm")
+store_address = soup.find_all("refine_lotno_addr")
+
 
 # 정보를 출력해주는 부분
 count = 0
@@ -27,9 +31,36 @@ for i in store_name:
     print("================================")
     count = count + 1
 
+#미리 생성한 DB와 연결(한글 입력을 위해 charset='utf8' 사용)
+    connection = MySQLdb.connect(
+        user="root",
+        passwd="khd10810",
+        host="localhost",
+        db="story",
+        charset='utf8'
+    )
+# Cursor의 사용으로 SQL 명령어를 DB에 전송 가능
+    cursor = connection.cursor()
+
+    #받을 때마다 겹치는 것을 막기 위해 테이블 제거 후 재생성
+    cursor.execute("DROP TABLE IF EXISTS eatplace")
+    cursor.execute("CREATE TABLE eatplace (id      int     not null      auto_increment,  name    varchar(100)    not null,  address varchar(100)    not null, primary key(id)) charset=utf8")
+
 if(count == 0):
     print("해당 시군에는 검색 결과가 존재하지 않습니다.")
 else:
     print() 
     print("결과 갯수 : " + str(count))
     print("검색이 완료되었습니다.")
+
+    
+   
+
+    # 데이터의  DB 저장
+    for num in range(len(store_name)):
+        order = f'INSERT INTO eatplace (name, address) VALUES (\'{store_name[num].string}\',\'{store_address[num].string}\')'
+        cursor.execute(order)
+        connection.commit()
+    print("데이터베이스 저장이 완료되었습니다.")
+    
+    connection.close()
