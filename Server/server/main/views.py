18f21f2c605dc037b9c8366f53b28f7a8e15bdd8
lungnahahd <jyoungjk33@gmail.com>
2021-01-10 00:50:34 +0900

    Fix_views.py(주석 처리)

--- Server/server/main/views.py
+++ Server/server/main/views.py
@@ -12,28 +12,24 @@ from bs4 import BeautifulSoup
 import requests
 from django.http import HttpResponse
 
+# 인증키를 입력
 key_value ='b56169eae0274d53943d1431cc14a41a'
+# 여러 가지 기본 정보들을 입력
 detail_value = '&Type=xml&pIndex=1&pSize=100&SIGUN_NM='
+# API 중 원하는 정보만을 검색해서 받는 부분
 search = input('원하는 경기도 시군명을 입력해주세요:)')
 final_url = 'https://openapi.gg.go.kr/TourismRestaurant?Key='+ key_value + detail_value + urllib.parse.quote_plus(search)
 request = final_url
-#@api_view(['GET'])
+
+# 웹으로 API 데이터를 전달 - 지금은 연습이므로 localhost 사용
 def store_list(request):
     req = requests.get(final_url)
     html=req.text
     soup = BeautifulSoup(html,'html.parser')
     store_name = soup.find_all("bizplc_nm")
     store_address = soup.find_all("refine_lotno_addr")
+    # 웹으로 데이터를 전달하는 부분
     result = ''
     for i in range(len(store_name)):
         result = result + f'<p>{store_name[i]} {store_address[i]}</p>'
-    response = urllib.request.urlopen(final_url)
-    byte_data = response.read()
-    text_data = byte_data.decode('utf-8')
     return HttpResponse(result)
-
-
-
-# class WonderViewSet(viewsets.ModelViewSet):
-#     queryset = Wonder.objects.all()
-#     serializer_class = WonderSerializer
